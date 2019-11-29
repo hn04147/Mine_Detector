38c1d2a7a2fb4860a343757b82a3866a1f496c35
ybear21 <55481772+ybear21@users.noreply.github.com>
2019-11-30 00:58:29 +0900

    Update main.py
    infinite roop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import random
 from pygame.locals import *
 from image import *
 
@@ -46,8 +47,20 @@ while True:
             elif mine_array[j][i]==1:
                 screen.blit(blank_clicked,(i*50,j*50))
             
-    screen.blit(mine,(250,350))
-    screen.blit(blank_flag,(100,100))
+
+    mine_list=[]
+    mine_n=0
+
+    while mine_n<11:
+        mine_x=random.randrange(0,11)
+        mine_y=random.randrange(0,11)
+        screen.blit(mine,(mine_x*50,mine_y*50+100))
+        mine_list.append(mine_x)
+        mine_list.append(mine_y)
+        mine_n=mine_n+1
+    
+    print(mine_list)
+
 
     pygame.display.flip()
     clock.tick(TARGET_FPS)

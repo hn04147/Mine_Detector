72f84bf5a5fa6512502dd33a5a20fc254a2491ce
hn04147 <hn04147@naver.com>
2019-11-30 00:09:36 +0900

    ok

--- image.py
+++ image.py
@@ -1,6 +1,6 @@
 import pygame
 
-blank = pygame.image.load('blank.jpg')
-blank_clicked = pygame.image.load('blank_clicked.png')
-mine = pygame.image.load('mine.jpg')
-blank_flag = pygame.image.load('blank_flag.jpg')
+blank = pygame.image.load('image/blank.jpg')
+blank_clicked = pygame.image.load('image/blank_clicked.png')
+mine = pygame.image.load('image/mine.jpg')
+blank_flag = pygame.image.load('image/blank_flag.jpg')

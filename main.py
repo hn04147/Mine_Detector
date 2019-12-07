3c0f2c4df5b09c6a7581832d42152df55d1d881c
hn04147 <hn04147@naver.com>
2019-12-07 21:43:25 +0900

    asdf

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ crashed = True
 
 
 #global
-MINE_NUM=20
-row = 20
-col = 20
+MINE_NUM=10
+row = 10
+col = 10
 
 #background color
 WHITE=(255,255,255)
@@ -39,15 +39,16 @@ after_click=[[0]*(col+2) for i in range(row+2)]
 before_click=[[0]*(col+2) for i in range(row+2)]
 
 #지뢰 10개 랜덤으로 배치 0->-1
-index=0
-while index < MINE_NUM:
-    randomx = random.randrange(0,row)
-    randomy = random.randrange(0,col)
-    if before_click[randomx][randomy] == 0:
-        before_click[randomx][randomy] = -1
-        index = index + 1
+def random_mine():
+    index=0
+    while index < MINE_NUM:
+        randomx = random.randrange(0,row)
+        randomy = random.randrange(0,col)
+        if before_click[randomx][randomy] == 0:
+            before_click[randomx][randomy] = -1
+            index = index + 1
 
-
+#클릭한 칸 여는 함수
 def open_blank(i, j):
     if before_click[i][j] == 0 and after_click[i][j] != -1: 
         after_click[i][j] = -1
@@ -66,103 +67,52 @@ def open_blank(i, j):
         gameover()
         crashed = False
 
+#before_click array에 수 입력
+def calculate_before_click():
+    for i in range (0, row):
+        for j in range (0, col):
+            if before_click[i][j] != -1:
+                index = 0
+                if i!=0:
+                    if before_click[i-1][j] == -1: index = index + 1
+                if i!=0 and j!=col-1:
+                    if before_click[i-1][j+1] == -1: index = index + 1
+                if j!=col-1:
+                    if before_click[i][j+1] == -1: index = index + 1
+                if i!=row-1 and j!=col-1:
+                    if before_click[i+1][j+1] == -1: index = index + 1
+                if i!=row-1:
+                    if before_click[i+1][j] == -1: index = index + 1
+                if i!=row-1 and j!=0:
+                    if before_click[i+1][j-1] == -1: index = index + 1
+                if j!=0:
+                    if before_click[i][j-1] == -1: index = index + 1
+                if i!=0 and j!=0:
+                    if before_click[i-1][j-1] == -1: index = index + 1
+                before_click[i][j] = index
+
+#게임판 출력
+def blit_game():
+    for i in range (0,col):
+        for j in range (0,row):
+            if after_click[j][i]==0: screen.blit(blank,(i*50,j*50))
+            elif after_click[j][i]==-1: screen.blit(blank_clicked,(i*50,j*50))
+            elif after_click[j][i]==10: screen.blit(mine,(i*50,j*50))
+            elif after_click[j][i]==9: screen.blit(blank_flag,(i*50,j*50))
+            elif after_click[j][i]==1: screen.blit(num1, (i*50,j*50))
+            elif after_click[j][i]==2: screen.blit(num2, (i*50,j*50))
+            elif after_click[j][i]==3: screen.blit(num3, (i*50,j*50))
+            elif after_click[j][i]==4: screen.blit(num4, (i*50,j*50))
+            elif after_click[j][i]==5: screen.blit(num5, (i*50,j*50))
+            elif after_click[j][i]==6: screen.blit(num6, (i*50,j*50))
+            elif after_click[j][i]==7: screen.blit(num7, (i*50,j*50))
+            elif after_click[j][i]==8: screen.blit(num8, (i*50,j*50))
+
 def gameover():
     tkinter.messagebox.showinfo("GAME OVER","GAME OVER")
-    
-    
-################################################################################
-        
-#공백 주변에 있는 지뢰의 수를 배열에 입력
-#1) 모서리에 걸치지 않는 칸부터 설정
-# _________
-# | * * * |
-# | * * * |
-# | * * * |
-# ---------
-for i in range (1,row-1):
-    for j in range (1,col-1):
-        if before_click[i][j]!=-1:
-            index = 0
-            for y in range (j-1, j+2):
-                if before_click[i-1][y]==-1:
-                    index = index + 1
-                if before_click[i+1][y]==-1:
-                    index = index + 1
-            if before_click[i][j-1]==-1:
-                index = index + 1
-            if before_click[i][j+1]==-1:
-                index = index + 1
-            before_click[i][j]=index
-#2) 꼭짓점칸 설정
-#  * ----- *
-#  |       |
-#  |       |
-#  |       |
-#  * ----- *
-#(0,0):
-if before_click[0][0]!=-1:
-    if before_click[0][1]==-1: before_click[0][0]+=1
-    if before_click[1][1]==-1: before_click[0][0]+=1
-    if before_click[1][0]==-1: before_click[0][0]+=1
-#(0,col-1):
-if before_click[0][col-1]!=-1:
-    if before_click[0][col-2]==-1: before_click[0][col-1]+=1
-    if before_click[1][col-2]==-1: before_click[0][col-1]+=1
-    if before_click[1][col-1]==-1: before_click[0][col-1]+=1
-#(row-1,0):
-if before_click[row-1][0]!=-1:
-    if before_click[row-2][0]==-1: before_click[row-1][0]+=1
-    if before_click[row-2][1]==-1: before_click[row-1][0]+=1
-    if before_click[row-1][1]==-1: before_click[row-1][0]+=1
-#(row-1,col-1):
-if before_click[row-1][col-1]!=-1:
-    if before_click[row-2][col-2]==-1: before_click[row-1][col-1]+=1
-    if before_click[row-2][col-1]==-1: before_click[row-1][col-1]+=1
-    if before_click[row-1][col-2]==-1: before_click[row-1][col-1]+=1
-#3) 모서리칸 설정
-#    * * * 
-#  *       *
-#  *       *
-#  *       *
-#    * * *
-#UPSIDE
-for i in range (1,col-1):
-    if before_click[0][i]!=-1:
-        if before_click[0][i-1]==-1: before_click[0][i]+=1
-        if before_click[0][i+1]==-1: before_click[0][i]+=1
-        for j in range (i-1, i+2):
-            if before_click[1][j]==-1: before_click[0][i]+=1
-#DOWNSIDE
-for i in range (1,col-1):
-    if before_click[row-1][i]!=-1:
-        if before_click[row-1][i-1]==-1: before_click[row-1][i]+=1
-        if before_click[row-1][i+1]==-1: before_click[row-1][i]+=1
-        for j in range (i-1, i+2):
-            if before_click[row-2][j]==-1: before_click[row-1][i]+=1
-#LEFTSIDE
-for i in range (1, row-1):
-    if before_click[i][0]!=-1:
-        if before_click[i-1][0]==-1: before_click[i][0]+=1
-        if before_click[i+1][0]==-1: before_click[i][0]+=1
-        for j in range (i-1, i+2):
-            if before_click[j][1]==-1: before_click[i][0]+=1
-#RIGHTSIDE
-for i in range (1, row-1):
-    if before_click[i][col-1]!=-1:
-        if before_click[i-1][col-1]==-1: before_click[i][col-1]+=1
-        if before_click[i+1][col-1]==-1: before_click[i][col-1]+=1
-        for j in range (i-1, i+2):
-            if before_click[j][col-2]==-1: before_click[i][col-1]+=1
-            
-################################################################################
 
-
-
-'''for i in range (0,row):
-    for j in range (0,col):
-        print("%3d"%before_click[i][j],end=' ')
-    print("\n")'''
-
+random_mine()
+calculate_before_click()
 
 while crashed:
     for event in pygame.event.get():
@@ -185,43 +135,10 @@ while crashed:
             y = mousex//50
             x = mousey//50
             after_click[x][y]=9
-            #print array
-            '''print("mine_array:")
-            for i in range (0,row):
-                for j in range (0,col):
-                    print("%3d"%after_click[i][j], end=' ')
-                print("\n")'''
             
-    
 
     #이미지 출력
-    for i in range (0,col):
-        for j in range (0,row):
-            if after_click[j][i]==0:
-                screen.blit(blank,(i*50,j*50))
-            elif after_click[j][i]==-1:
-                screen.blit(blank_clicked,(i*50,j*50))
-            elif after_click[j][i]==10:
-                screen.blit(mine,(i*50,j*50))
-            elif after_click[j][i]==9:
-                screen.blit(blank_flag,(i*50,j*50))
-            elif after_click[j][i]==1:
-                screen.blit(num1, (i*50,j*50))
-            elif after_click[j][i]==2:
-                screen.blit(num2, (i*50,j*50))
-            elif after_click[j][i]==3:
-                screen.blit(num3, (i*50,j*50))
-            elif after_click[j][i]==4:
-                screen.blit(num4, (i*50,j*50))
-            elif after_click[j][i]==5:
-                screen.blit(num5, (i*50,j*50))
-            elif after_click[j][i]==6:
-                screen.blit(num6, (i*50,j*50))
-            elif after_click[j][i]==7:
-                screen.blit(num7, (i*50,j*50))
-            elif after_click[j][i]==8:
-                screen.blit(num8, (i*50,j*50))
-
+    blit_game()
 
     pygame.display.flip()
     clock.tick(TARGET_FPS)
